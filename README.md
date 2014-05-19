9df210541f8081ff905508c8265bd506d0892274
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2014-05-19 10:38:25 +0900

    for Linux

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ pomodorocl
 ==========
 
 Command Line Pomodoro TImer
+
+Supported OS: Linux, Mac
+
+USAGE
+------
+1. write google account settings into `~/.pomodoro`
+```
+[User]
+email = john@foo.com
+password = abracadabra
+```
+2. create calendar for logging at Google Calendar
+3. then, you can use as `python pomodoro.py 25 --message "first task" --cal created_calendar`
--- notifysend/notifysend.py
+++ notifysend/notifysend.py
@@ -1,8 +1,20 @@
 __author__ = 'ogaki'
 
-import pynotify
+try:
+    import pynotify
+except ImportError:
+    import pync
+
 
 class Notifier:
+    """for Mac"""
+    def __init__(self, message=""):
+        self.message = message
+    def send(self):
+        pync.Notifier.notify(self.message)
+
+class LinNotifier:
+    """for Linux"""
     def __init__(self, message=""):
         pynotify.init(message)
         self.n = pynotify.Notification(message, message)
